6713f3caa2a3b8559b4f62a7ebf8f1e8b9677bd4
한재경 <hjg000223@gmail.com>
2024-11-07 15:04:16 +0900

    feat: UserService findByLoginId() 추가

--- src/main/java/com/fitable/backend/user/service/UserService.java
+++ src/main/java/com/fitable/backend/user/service/UserService.java
@@ -29,4 +29,8 @@ public class UserService {
 
         return userRepository.save(user);
     }
+
+    public Optional<User> findByLoginId(String loginId) {
+        return userRepository.findByLoginId(loginId);
+    }
 }
