5d10750c188a1ee029ef003d96079477453696a4
한재경 <hjg000223@gmail.com>
2024-11-07 17:47:33 +0900

    feat: CORS 허용

--- src/main/java/com/fitable/backend/config/SecurityConfig.java
+++ src/main/java/com/fitable/backend/config/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -19,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("**").permitAll() // 모든 사용자에게 모든 경로 접근 허용, 추후 특정 경로로 고쳐주기
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
@@ -26,5 +32,18 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable); // CSRF 보호 비활성화
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://fitable-frontend.vercel.app")); // 허용할 출처 설정
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드 설정
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type")); // 허용할 헤더 설정
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
