2803fa85270fa6e24790a7a448dd831a223f5d49
한재경 <hjg000223@gmail.com>
2024-11-08 11:57:46 +0900

    feat: SecurityConfig 3000포트 CORS 비설정

--- src/main/java/com/fitable/backend/config/SecurityConfig.java
+++ src/main/java/com/fitable/backend/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);  // 자격 증명 허용
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:5173/"));  // 도메인 설정
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000\", http://localhost:5173/"));  // 도메인 설정
         config.setAllowedMethods(Collections.singletonList("*"));  // 모든 HTTP 메서드 허용
         config.setAllowedHeaders(Collections.singletonList("*"));  // 모든 헤더 허용
 
