0dd72a59df4b112a6295e668eee563816f5efedc
한재경 <hjg000223@gmail.com>
2024-11-08 11:58:44 +0900

    feat: UserService Jwt, CustiomUserDetailsService 추가

--- src/main/java/com/fitable/backend/user/service/UserService.java
+++ src/main/java/com/fitable/backend/user/service/UserService.java
@@ -2,13 +2,18 @@ package com.fitable.backend.user.service;
 
 import com.fitable.backend.user.dto.RegisterRequest;
 import com.fitable.backend.user.entity.User;
+import com.fitable.backend.user.exception.UserNotFoundException;
 import com.fitable.backend.user.repository.UserRepository;
+import com.fitable.backend.user.util.JwtTokenUtil;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -17,8 +22,9 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
-    @Getter
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final CustomUserDetailsService customUserDetailsService;
 
     // 회원 가입 메서드
     public void registerUser(RegisterRequest registerRequest) {
@@ -41,6 +47,38 @@ public class UserService {
     // 사용자 존재 여부 확인
     public boolean existsByLoginId(String loginId) {
         return userRepository.existsByLoginId(loginId);
+    }
+
+    public Map<String, String> login(String loginId, String password) {
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(loginId);
+
+        // 사용자 유효성 검증
+        validateUser(userDetails, password);
+
+        // JWT 토큰 생성
+        String accessToken = jwtTokenUtil.generateToken(userDetails);
+        String refreshToken = jwtTokenUtil.generateRefreshToken(userDetails);
+        log.info("JWT Token generated for user: {}", loginId);
+
+        // AccessToken과 RefreshToken을 Map으로 반환
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("accessToken", accessToken);
+        tokens.put("refreshToken", refreshToken);
+
+        return tokens;
+    }
+
+    // 사용자 비밀번호 및 존재 여부 검증 메서드
+    private void validateUser(UserDetails userDetails, String rawPassword) {
+        if (userDetails == null) {
+            log.warn("Login failed. User not found with login ID: {}", rawPassword);
+            throw new UserNotFoundException("Login ID does not exist");
+        }
+
+        if (!passwordEncoder.matches(rawPassword, userDetails.getPassword())) {
+            log.warn("Invalid password for user: {}", userDetails.getUsername());
+            throw new RuntimeException("Invalid password");
+        }
     }
 
     // 사용자 로그인 ID로 조회
