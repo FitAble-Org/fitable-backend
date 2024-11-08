912b1e1659d603880680bd9839c06dda7dd2159c
한재경 <hjg000223@gmail.com>
2024-11-08 16:18:00 +0900

    feat: RecommendedExerciseService 추가

---
+++ src/main/java/com/fitable/backend/publicdata/service/RecommendedExerciseService.java
@@ -0,0 +1,20 @@
+package com.fitable.backend.publicdata.service;
+
+import com.fitable.backend.publicdata.entity.RecommendedExercise;
+import com.fitable.backend.publicdata.repository.RecommendedExerciseRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class RecommendedExerciseService {
+
+    @Autowired
+    private RecommendedExerciseRepository recommendedExerciseRepository;
+
+    @Transactional
+    public void saveAllExercises(List<RecommendedExercise> exercises) {
+        recommendedExerciseRepository.saveAll(exercises);
+    }
+}
