f50d3910832f2b33bce1610446793ffa246e8a16
한재경 <hjg000223@gmail.com>
2024-11-08 16:57:49 +0900

    feat: RecommendedExercise 복구

--- src/main/java/com/fitable/backend/publicdata/repository/RecommendedExerciseRepository.java
+++ src/main/java/com/fitable/backend/publicdata/repository/RecommendedExerciseRepository.java
@@ -1,5 +1,6 @@
 package com.fitable.backend.publicdata.repository;
 
+import com.fitable.backend.publicdata.entity.RecommendedExercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecommendedExerciseRepository extends JpaRepository<RecommendedExercise, Long> {
--- src/main/java/com/fitable/backend/publicdata/service/RecommendedExerciseService.java
+++ src/main/java/com/fitable/backend/publicdata/service/RecommendedExerciseService.java
@@ -1,5 +1,6 @@
 package com.fitable.backend.publicdata.service;
 
+import com.fitable.backend.publicdata.entity.RecommendedExercise;
 import com.fitable.backend.publicdata.repository.RecommendedExerciseRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
