996bc673fd37b055f06c42d52ef015d5a9ca55bf
한재경 <hjg000223@gmail.com>
2024-11-08 11:33:01 +0900

    feat: UserService 회원가입 시 PasswordEncoder 암호화 및 검증로직 추가

--- src/main/java/com/fitable/backend/user/service/UserService.java
+++ src/main/java/com/fitable/backend/user/service/UserService.java
@@ -1,10 +1,11 @@
 package com.fitable.backend.user.service;
 
-import com.fitable.backend.user.dto.UserRequest;
+import com.fitable.backend.user.dto.RegisterRequest;
 import com.fitable.backend.user.entity.User;
 import com.fitable.backend.user.repository.UserRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +13,37 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
     @Getter
     private final PasswordEncoder passwordEncoder;
 
-    public User registerUser(UserRequest userRequest) {
-        User user = new User();
-        user.setLoginId(userRequest.getLoginId());
-        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
-        user.setAgeGroup(userRequest.getAgeGroup());
-        user.setGender(userRequest.getGender());
-        user.setDisabilityType(userRequest.getDisabilityType());
-        user.setDisabilityLevel(userRequest.getDisabilityLevel());
+    // 회원 가입 메서드
+    public void registerUser(RegisterRequest registerRequest) {
+        if (existsByLoginId(registerRequest.getLoginId())) {
+            log.warn("Registration failed. Login ID already exists: {}", registerRequest.getLoginId());
+            throw new IllegalArgumentException("Login ID already exists");
+        }
 
-        return userRepository.save(user);
+        User newUser = new User(registerRequest.getLoginId(),
+                passwordEncoder.encode(registerRequest.getPassword()),
+                registerRequest.getAgeGroup(),
+                registerRequest.getGender(),
+                registerRequest.getDisabilityType(),
+                registerRequest.getDisabilityLevel());
+
+        userRepository.save(newUser);
+        log.info("User registered successfully: {}", registerRequest.getLoginId());
     }
 
+    // 사용자 존재 여부 확인
+    public boolean existsByLoginId(String loginId) {
+        return userRepository.existsByLoginId(loginId);
+    }
+
+    // 사용자 로그인 ID로 조회
     public Optional<User> findByLoginId(String loginId) {
         return userRepository.findByLoginId(loginId);
     }
