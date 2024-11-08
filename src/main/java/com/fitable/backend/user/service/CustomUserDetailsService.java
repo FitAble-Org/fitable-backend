d240c9aebfee79372c56d9eba868cb7e18c4a860
한재경 <hjg000223@gmail.com>
2024-11-08 11:57:25 +0900

    feat: CustomUserDetailsService 추가

---
+++ src/main/java/com/fitable/backend/user/service/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.fitable.backend.user.service;
+
+import com.fitable.backend.user.entity.User;
+import com.fitable.backend.user.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //userRepository.findByLoginId(loginId): 일반적인 사용자 정보 조회 및 서비스 로직에서 사용.
+    //customUserDetailsService.loadUserByUsername(loginId): Spring Security의 인증 및 권한 부여 로직에서 사용.
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByLoginId(loginId);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with loginId: " + loginId);
+        }
+
+        // 사용자 권한 설정
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.get().getLoginId(),
+                user.get().getPassword(),
+                authorities
+        );
+    }
+}
