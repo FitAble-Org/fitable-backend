6aa6a175021402a8c415fe009f51529a56148406
한재경 <hjg000223@gmail.com>
2024-11-11 12:46:45 +0900

    feat: FacilityController getNearbyFacilities()에서 시설목록 + GPT 응답 반환하도록 수정

--- src/main/java/com/fitable/backend/facilitytraining/controller/FacilityController.java
+++ src/main/java/com/fitable/backend/facilitytraining/controller/FacilityController.java
@@ -1,12 +1,11 @@
 package com.fitable.backend.facilitytraining.controller;
 
+import com.fitable.backend.facilitytraining.dto.FacilityResponseWithGpt;
 import com.fitable.backend.facilitytraining.dto.LocationRequest;
-import com.fitable.backend.facilitytraining.dto.FacilityResponse;
 import com.fitable.backend.facilitytraining.service.FacilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/facilities")
@@ -16,7 +15,7 @@ public class FacilityController {
     private FacilityService facilityService;
 
     @PostMapping("/nearby")
-    public List<FacilityResponse> getNearbyFacilities(@RequestBody LocationRequest locationRequest) {
+    public Mono<FacilityResponseWithGpt> getNearbyFacilities(@RequestBody LocationRequest locationRequest) {
         return facilityService.findFacilitiesWithinRadius(locationRequest);
     }
 }
