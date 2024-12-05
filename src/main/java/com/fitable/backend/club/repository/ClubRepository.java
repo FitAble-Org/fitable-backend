36d2089b97d9e20d7b228571af6cbd6d45327e98
hanjaegyeong <hjg000223@naver.com>
2024-12-06 03:24:06 +0900

    feat: ClubRepository - 조건별 검색기능 추가

--- src/main/java/com/fitable/backend/club/repository/ClubRepository.java
+++ src/main/java/com/fitable/backend/club/repository/ClubRepository.java
@@ -4,6 +4,8 @@ import com.fitable.backend.club.entity.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface ClubRepository extends JpaRepository<Club, Integer> {
+public interface ClubRepository extends JpaRepository<Club, Long> {
+    List<Club> findByCtprvnNm(String ctprvnNm);
+    List<Club> findByTroblTyNm(String troblTyNm);
     List<Club> findByCtprvnNmAndTroblTyNm(String ctprvnNm, String troblTyNm);
 }
